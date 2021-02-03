0c17b33aa0c9a39ad9759ef1453ed3c2706180df
danish469 <dmohammad469@gmail.com>
2021-02-03 12:29:54 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
+import { MatButtonModule } from "@angular/material";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { TodoComponent } from "./todo/todo.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule,MatButtonModule],
   declarations: [AppComponent, HelloComponent, TodoComponent],
   bootstrap: [AppComponent]
 })
