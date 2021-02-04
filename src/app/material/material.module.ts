118c712e2303c6969c95bad82521eba6c71a238c
danish469 <dmohammad469@gmail.com>
2021-02-04 12:00:26 +0530

    3 files modified

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,14 @@ import { MatButtonToggleModule } from "@angular/material/button-toggle";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
-  MatInputModule
+  MatInputModule,
+  MatFormFieldModule
 ];
 @NgModule({
   imports: [MaterialComponents],
