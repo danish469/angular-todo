fdb0dea30312b124d32c6e80ca5ee38d2d33ee04
danish469 <dmohammad469@gmail.com>
2021-02-04 10:19:18 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { MatButtonModule } from "@angular/material";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { TodoComponent } from "./todo/todo.component";
