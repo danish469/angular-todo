0f3697d99c86ba7f2be2a8917a66bdbe0008cc42
danish469 <dmohammad469@gmail.com>
2021-02-04 11:50:24 +0530

    4 files modified

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from "@angular/core";
 import { MatButtonToggleModule } from "@angular/material/button-toggle";
-// import {} from "@angular/material/button";
-import { MatButtonModule, MatIconModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
-  MatIconModule
+  MatIconModule,
+  MatInputModule
 ];
 @NgModule({
   imports: [MaterialComponents],
