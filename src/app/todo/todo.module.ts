b1282d4daa7fcfb387f2c76641883cd2c20d6313
danish469 <dmohammad469@gmail.com>
2021-02-04 12:06:47 +0530

    2 files modified

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
-import { MatButtonModule } from "@angular/material/button";
 import { MaterialModule } from "../material/material.module";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatButtonModule, MaterialModule],
+  imports: [BrowserModule, FormsModule, MaterialModule],
   declarations: []
 })
 export class TodoModule {}
